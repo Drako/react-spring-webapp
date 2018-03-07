ff466e22cdb4a00fb29e80730f6e4c532d9db1f6
Felix Bytow <felix@staffbase.com>
2018-03-07 01:10:50 +0100

    :sparkles: added actuator and fixed className in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className={styles.App}>
-        <header className="App-header">
+        <header className={styles["App-header"]}>
           <img src={logo} className={styles["App-logo"]} alt="logo" />
           <h1 className={styles["App-title"]}>Welcome to React</h1>
         </header>
