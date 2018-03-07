3175f249e5fff07772f432dd72e6aa04b2be5c06
Felix Bytow <drako@drako.guru>
2018-03-07 17:34:39 +0100

    :recycle: replaced provided react icon with fontawesome one

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import styles from './App.css';
+import FontAwesome from '@fortawesome/fontawesome';
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import Brands from '@fortawesome/fontawesome-free-brands';
+
+FontAwesome.library.add(Brands);
 
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
         <header className={styles["App-header"]}>
-          <img src={logo} className={styles["App-logo"]} alt="logo" />
+          <FontAwesomeIcon icon={['fab', 'react']} spin size='5x' className={styles["App-logo"]} />
           <h1 className={styles["App-title"]}>Welcome to React</h1>
         </header>
       </div>
